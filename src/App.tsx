12fbe019297b3ee007931108c63ffcf4541cd24b
Kevinchuka <kevinvrancken2002@gmail.com>
2025-12-29 20:42:40 -0300

    Version 1 - Finalizado

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useWindowSize } from "react-use"
+import PokemonDisplay from "./components/pokemon-display"
+import PokemonForm from "./components/pokemon-form"
+import PokemonResult from "./components/pokemonResult"
+import { GameState, useGameManager } from "./hooks/use-game-manager"
+import ReactConfetti from "react-confetti"
+
+const App = () => {
+  const { loadNewPokemon, pokemon, error, isLoading, gameState, handlePokemonNameSubmit } = useGameManager()
+
+  const { width, height } = useWindowSize();
+
+  if (error) {
+    return <div className="alert alert-danger text-centetr">Error: {error}</div>;
+  }
+
+  return (
+    <div className="container mx-auto my-5">
+      {gameState === GameState.Correct && (
+        <ReactConfetti
+          width={width}
+          height={height}
+          numberOfPieces={300}
+          recycle={false}
+        />
+      )}
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-8 col-lg-6">
+          <PokemonDisplay pokemon={pokemon} isLoading={isLoading} gameState={gameState} />
+          <PokemonForm
+            gameState={gameState}
+            handlePokemonNameSubmit={handlePokemonNameSubmit} />
+          <PokemonResult loadNewPokemon={loadNewPokemon}
+            gameState={gameState} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/services/pokemon.service.ts
@@ -0,0 +1,46 @@
+
+import type { Pokemon } from "../types/pokemon.interface";
+import { getRandomIntInclusive } from "../lib/random-number";
+
+const POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon';
+const MAX_POKEMON_COUNT = 151;
+
+export const getRandomPokemon = async (): Promise<Pokemon> => {
+    const randomId = getRandomIntInclusive(1, MAX_POKEMON_COUNT);
+    const response = await fetch(`${POKEMON_API_URL}/${randomId}`);
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch Pokémon with ID ${randomId}`);
+}
+
+const data = await response.json();
+    return {
+        id: data.id,
+        name: data.name,
+        image: data.sprites.other["official-artwork"].front_default,
+    };
+}
+
+const normalizePokemonName = (name: string): string => {
+    return name
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // Remove diacritics
+    .replace(/[^a-z0-9]+/g, ""); // Replace non-alphanumeric characters
+};
+
+const isPokemonNameValid = (
+    pokemonName: string,
+    userInput:string
+): boolean => {
+    const normalizedPokemonName = normalizePokemonName(pokemonName);
+    const normalizedUserInput = normalizePokemonName(userInput);
+
+    return normalizedPokemonName === normalizedUserInput;
+}
+
+export const pokemonService = {
+    getRandomPokemon,
+    isPokemonNameValid
+};
